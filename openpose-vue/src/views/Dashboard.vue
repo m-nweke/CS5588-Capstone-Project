06bb538adf91b06e510f7426499c4ad7f63592e9
m-nweke <mnweke@mail.umkc.edu>
2024-04-03 20:12:18 -0500

    Add name to dashboard

--- openpose-vue/src/views/Dashboard.vue
+++ openpose-vue/src/views/Dashboard.vue
@@ -1,11 +1,14 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-Dash
+Hello {{ name }}
 </template>
 
 <style scoped>
 
 </style>
+
+<script setup lang="ts">
+import { auth } from '@/main';
+
+const user = auth.currentUser;
+const name = auth.currentUser?.displayName
+</script>
