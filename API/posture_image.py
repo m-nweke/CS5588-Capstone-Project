9cbc8c1758a1ae8adab416648042aef0c181348c
m-nweke <mnweke@mail.umkc.edu>
2024-04-18 17:57:27 -0500

    Fix import

--- API/posture_image.py
+++ API/posture_image.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 import util
 from config_reader import config_reader
-from scipy.ndimage.filters import gaussian_filter
+from scipy.ndimage import gaussian_filter
 from model import get_testing_model
 
 tic = 0
